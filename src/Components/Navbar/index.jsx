75a79d1fdc664f2526c129e95dcf3ce4bee8f64f
souvikTito <souvik_tito@outlook.com>
2023-11-06 00:48:24 +0530

    first Commit

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.css";
+import MenuIcon from "@mui/icons-material/Menu";
+function Navbar() {
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-light ${styles.nav_container}`}
+    >
+      <div className={`${styles.nav_left}`}>
+        <a href="#home">Souvik</a>
+      </div>
+      <button className={`d-md-none btn me-3 ${styles.hamburger}`}>
+        <MenuIcon />
+      </button>
+
+      <div className={`${styles.nav_right} ${styles.mobile_nav}`}>
+        <a href="#about" className={`${styles.nav_link}`}>
+          <span></span>About
+        </a>
+        <a href="#experience" className={`${styles.nav_link}`}>
+          <span>02.</span>Experience
+        </a>
+        <a href="#work" className={`${styles.nav_link}`}>
+          <span>03.</span>Work
+        </a>
+        <a href="#contact" className={`${styles.nav_link}`}>
+          <span>04.</span>Contact
+        </a>
+        <button className={`${styles.nav_resume_btn}`}>Resume</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
